0641b7bb55d9664fdf76ab86a152d27287d6af1b
Derek Thurber <derek.thurber@northwestern.edu>
2017-04-14 12:43:06 -0500

    added guides and added resources page

--- js/init.js
+++ js/init.js
@@ -25,18 +25,60 @@ mpApp.config(function($routeProvider) {
             activetab: 'research-question'
         })
 
-        // route for the about page
+        // route for the research question page
         .when('/research-question', {
             templateUrl : 'guides/research-question.html',
             controller  : 'mainController',
             activetab: 'research-question'
         })
 
-        // route for the contact page
+        // route for the abstract page
         .when('/abstract', {
             templateUrl : 'guides/abstract.html',
             controller  : 'mainController',
             activetab: 'abstract'
+        })
+        
+        // route for the introduction and background page
+        .when('/intro-background', {
+            templateUrl : 'guides/intro-background.html',
+            controller  : 'mainController',
+            activetab: 'intro-background'
+        })
+  
+        // route for the literature review page
+        .when('/literature-review', {
+            templateUrl : 'guides/literature-review.html',
+            controller  : 'mainController',
+            activetab: 'literature-review'
+        })
+  
+        // route for the data collection methods page
+        .when('/data-collection', {
+            templateUrl : 'guides/data-collection.html',
+            controller  : 'mainController',
+            activetab: 'data-collection'
+        })
+  
+        // route for the data results summary page
+        .when('/data-results', {
+            templateUrl : 'guides/data-results.html',
+            controller  : 'mainController',
+            activetab: 'data-results'
+        })
+  
+        // route for the data interpretation page
+        .when('/data-interpretation', {
+            templateUrl : 'guides/data-interpretation.html',
+            controller  : 'mainController',
+            activetab: 'data-interpretation'
+        })
+  
+        // route for the conclusion page
+        .when('/conclusion', {
+            templateUrl : 'guides/conclusion.html',
+            controller  : 'mainController',
+            activetab: 'conclusion'
         });
 });
 
